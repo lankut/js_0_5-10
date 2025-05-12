349c7e1e54509d1e5ff5a949bf9570452288a133
lankut <lankute@mail.ru>
2025-05-12 16:00:00 +0300

    js_0_5-10_1.3

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,3 +1,6 @@
 // Напишите функцию calculateAverage, которая принимает 2 числа и возвращает их среднее арифметическое
 
-function calculateAverage() {}
+function calculateAverage(a, b) {
+    const averageSum = (a + b) / 2
+    return averageSum;
+}
--- lesson-03/03.js
+++ lesson-03/03.js
@@ -1,3 +1,23 @@
 // Разработайте функцию findLargest, которая принимает три числа и возвращает наибольшее из них.
 
-function findLargest() {}
+function findLargest(a, b, c,) {
+    let max = 0;
+    if (a > b) {
+        max = a
+        if (max > c) {
+            return max
+        }
+        else {
+            return c
+        }
+    } else if (b > a) {
+        max = b
+        if (max > c) {
+            return max
+        }
+            else {
+                return c
+            }
+    }
+    return max
+}
